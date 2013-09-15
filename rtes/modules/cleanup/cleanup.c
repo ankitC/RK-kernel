d319cea36dc0f1b590032111048d078c9f556861
Akshita Jain <akshitaj@andrew.cmu.edu>
2013-09-15 15:26:55 -0400

    Adding cleanup.c which compiles

--- rtes/modules/cleanup/cleanup.c
+++ rtes/modules/cleanup/cleanup.c
@@ -1,10 +1,12 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <asm/unistd.h>
+#include <asm/current.h>
+
 
 MODULE_LICENSE("GPL");
 
-unsigned long *sys_call_table = (unsigned long *)0xc000ec84;
+unsigned long **sys_call_table = (unsigned long **) 0xc000ec84;
 
 asmlinkage int (*original_sys_exit)(int);
 
@@ -12,7 +14,7 @@ asmlinkage int our_fake_exit_function(int error_code)
 {
 	/*print message on console every time we are called*/
 	printk("HEY! sys_exit called with error_code=%d\n",error_code);
-
+	
 	/*call original sys_exit and return its value*/
 	return original_sys_exit(error_code);
 }
@@ -20,18 +22,22 @@ asmlinkage int our_fake_exit_function(int error_code)
 int init_module(void)
 {
 	/*store reference to the original sys_exit call*/
-	original_sys_exit =  &sys_call_table[__NR_exit];
+//	if (strstr (current->comm, "sh"))
+//	{
+		original_sys_exit = (void *) sys_call_table[__NR_exit];
 
-	/*manipulate sys_call_table to call our fake exit
+		/*manipulate sys_call_table to call our fake exit
 	 *     function instead*/
-	sys_call_table[__NR_exit] = (unsigned long) our_fake_exit_function;
-
+		sys_call_table[__NR_exit] = (unsigned long* ) our_fake_exit_function;
+//	}
 	return 0;
 }
 
 void cleanup_module(void)
 {
+//	if (strstr (current->comm, "sh"))
+//	{
 	/*restore original sys_exit*/
-	sys_call_table[__NR_exit] = (unsigned long) original_sys_exit;
-
+	sys_call_table[__NR_exit] = (unsigned long* ) original_sys_exit;
+//	}
 }
