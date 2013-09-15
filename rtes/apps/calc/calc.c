b4873dc7c1c1a7fcccfc94c5f0d2bcd521658c98
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-15 00:00:24 -0400

    Adding the calculator

--- rtes/apps/calc/calc.c
+++ rtes/apps/calc/calc.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <asm/unistd.h>
-//#include <linux/syscalls.h>
-#define __NR_calc 376
+#include <sys/syscall.h>
 
-__syscall3(long, calc, long, long, char)
+#define SCALE 1000
 
-int main(void)
+/*Wrapper for calc*/
+float calc(float num1, float num2, char operation)
 {
-	calc(3,4,'+');
-	printf("Calculator!\n");
-	return 0;
+	float result = 0;
+	long first = num1 * SCALE;
+	long second = num2 * SCALE;
+	int retval = syscall(__NR_calc, first, second, operation) ;
+		
+	switch(operation){
+
+		case '+':
+		case '-':
+			result = ((float) retval) / SCALE;
+			break;
+		case '*':
+			result = ((float) retval) / (SCALE * SCALE);
+			break;
+		case '/':
+			result = retval;
+			break;
+		default:
+			printf("Operation not supported\n");
+			break;
+	}
+	return result;
+}
+
+void main(int argc, char** argv){
+
+	float num1, num2;
+	float result = 0;
+	char op;
+	
+	num1 = atof(argv[1]);
+	num2 = atof(argv[3]);
+	op = *argv[2];
+	result=calc(num1, num2, op);
+	printf("Result=%f\n", result);
 }
--- rtes/kernel/calc.c
+++ rtes/kernel/calc.c
@@ -1,6 +1,37 @@
 #include <linux/kernel.h>
+#include <linux/linkage.h>
 #include <linux/syscalls.h>
+
+/*asmlinkage int sys_calc(long first, long second, char operation){
+
+  printk(KERN_DEBUG "TestCalc\n");
+  }*/
+
+
 SYSCALL_DEFINE3(calc, long, first, long, second, char, operation)
 {
-	printk(KERN_DEBUG "Test Calc\n");
+	long result = 0;
+
+	switch(operation){
+
+		case '+':
+			result = first + second;
+			break;
+		case '-':
+			result = first - second;
+			break;
+		case '*':
+			result = first * second;
+			break;
+		case '/':
+			result = first / second;
+			break;
+		default:
+			printk(KERN_WARNING "Operation not Supported.\n");
+			break;
+	}
+
+	printk(KERN_INFO "First Operand = %ld\nSecond Operand = %ld\n \
+			Operation = %c\nResult = %ld\n", first, second, operation, result);
+	return result;
 }
