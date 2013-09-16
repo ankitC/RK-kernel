8dcb5c4cddae22981ecacb823950ad18d41446e0
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-16 16:53:23 -0400

    Adding kernel/ps.c

--- arch/arm/include/asm/unistd.h
+++ arch/arm/include/asm/unistd.h
@@ -404,6 +404,7 @@
 #define __NR_setns			(__NR_SYSCALL_BASE+375)
 #define __NR_calc			(__NR_SYSCALL_BASE+376)
 #define __NR_count_processes (__NR_SYSCALL_BASE+377)
+#define __NR_list_processes	(__NR_SYSCALL_BASE+378)
 /*
  * The following SWIs are ARM private.
  */
--- rtes/kernel/ps.c
+++ rtes/kernel/ps.c
@@ -3,17 +3,56 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 #include <asm/uaccess.h>
-//static char msg[BUF_LEN];	/* The msg the device will give when asked */
-//static char *msg_Ptr;
+
+#define LINELENGTH 43
 
 SYSCALL_DEFINE0(count_processes)
 {
-	unsigned short numProcesses = 0;
+	unsigned short num_processes = 0;
 	struct task_struct *task;
 	read_lock(&tasklist_lock);
 	for_each_process (task)
-		numProcesses++;
+		num_processes++;
 	read_unlock(&tasklist_lock);
-	return numProcesses;
+	return num_processes;
+}
+
+SYSCALL_DEFINE1(list_processes, char*, user_buffer)
+{
+	static char *kernel_buffer;
+	struct task_struct *task;
+	unsigned long bytes_remaining = 0;
+	char* null_char= "\0";
+	int num_processes = 0;
+
+	
+	read_lock(&tasklist_lock);
+	for_each_process (task)
+		num_processes++;
+	
+	if((kernel_buffer=kmalloc(num_processes * LINELENGTH, \
+			GFP_KERNEL)) == NULL)
+	{
+		printk( KERN_DEBUG "Kmalloc error.\n");
+		return -1;
+	}
+
+	sprintf(kernel_buffer,"pid\tpr\tname\n");
+
+	for_each_process (task)
+	{
+		sprintf(kernel_buffer, "%s %i\t%i\t%s\n", kernel_buffer, task->pid,\
+			   	task->prio, task->comm);
+	}
+	read_unlock(&tasklist_lock);
+	
+	sprintf(kernel_buffer,null_char);
+
+	bytes_remaining = copy_to_user(user_buffer, kernel_buffer, \
+				strlen(kernel_buffer) );
+	kfree(kernel_buffer);
+	
+	return bytes_remaining;
 }
