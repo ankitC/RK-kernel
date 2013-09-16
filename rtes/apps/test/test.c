153a0eb60e191459a0ed157ffad6694cb0d27299
Akshita Jain <akshitaj@andrew.cmu.edu>
2013-09-16 18:00:48 -0400

    Working ps.c but yet to be debugged for errors

--- rtes/apps/test/test.c
+++ rtes/apps/test/test.c
@@ -4,18 +4,37 @@
 #include <asm/unistd.h>
 #include <sys/syscall.h>
 
+#define LISTLENGTH 43
+int list_processes(char* buffer)
+{
+	int retval = 0;
+	if ((retval = syscall(__NR_list_processes, buffer)) == 0)
+	{
+		printf("return %d\n", retval);
+		return 0;
+	}
+	else
+		return -1;
+}
+
 int count_processes()
 {
 	return syscall(__NR_count_processes);
 }
-
 
 int main(void)
 {
 //	int fd2 = open ("/sdcard/t.txt", O_RDWR|O_CREAT);
 //	printf("test, world! I am an Android app. %d\n", fd2);
 //	exit(0);
-	int i=(int)count_processes();
-	printf("The process count is %d\n",i);
+	int i = (int) count_processes();
+	char* buffer = calloc( LISTLENGTH, 1);
+
+	if (!list_processes(buffer))
+	{
+		printf("%s",buffer);
+	}
+
+	free(buffer);
 	return 0;
 }
