ae3b15def186c4d779032c578a785ea9a24c540d
Akshita Jain <akshitaj@andrew.cmu.edu>
2013-09-15 20:35:41 -0400

    cleanup.c with strstr

--- rtes/modules/cleanup/cleanup.c
+++ rtes/modules/cleanup/cleanup.c
@@ -1,4 +1,13 @@
-#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/module.h> 
+#include<linux/sched.h>
+#include <linux/rcupdate.h>
+#include <linux/fdtable.h>
+#include <linux/fs.h> 
+#include <linux/fs_struct.h>
+#include <linux/dcache.h>
+#include <linux/slab.h>
+#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <asm/unistd.h>
 #include <asm/current.h>
@@ -6,38 +15,56 @@
 
 MODULE_LICENSE("GPL");
 
-unsigned long **sys_call_table = (unsigned long **) 0xc000ec84;
+static char* comm = "";
+module_param(comm, charp, 0);
+MODULE_PARM_DESC(mystring, "A character string");
 
+unsigned long **sys_call_table = (unsigned long **) 0xc000ec84;
 asmlinkage int (*original_sys_exit)(int);
 
 asmlinkage int our_fake_exit_function(int error_code)
 {
-	/*print message on console every time we are called*/
-	printk("HEY! sys_exit called with error_code=%d\n",error_code);
-	
-	/*call original sys_exit and return its value*/
+
+	struct files_struct *current_files; 
+	struct fdtable *files_table;
+	int i=0;
+	struct path files_path;
+	char *cwd;
+	char *buf = (char *)kmalloc(GFP_KERNEL,100*sizeof(char));
+
+	printk(KERN_ALERT "INSIDE FAKE EXIT");
+	printk(KERN_ALERT "Name of the current task  %s\n", current->comm);
+	current_files = current->files;
+	files_table = files_fdtable(current_files);
+
+	if (strstr (current->comm, comm))
+	{
+		while(files_table->fd[i] != NULL)
+		{ 
+			files_path = files_table->fd[i]->f_path;
+			cwd = d_path(&files_path,buf,100*sizeof(char));
+
+			printk(KERN_ALERT "Open file with fd %d  %s", i, cwd);
+
+			i++;
+		}/*call original sys_exit and return its value*/}
 	return original_sys_exit(error_code);
 }
 
 int init_module(void)
 {
+
 	/*store reference to the original sys_exit call*/
-//	if (strstr (current->comm, "sh"))
-//	{
 		original_sys_exit = (void *) sys_call_table[__NR_exit];
 
 		/*manipulate sys_call_table to call our fake exit
-	 *     function instead*/
+		 *     function instead*/
 		sys_call_table[__NR_exit] = (unsigned long* ) our_fake_exit_function;
-//	}
-	return 0;
-}
+		return 0;
+	}
 
-void cleanup_module(void)
-{
-//	if (strstr (current->comm, "sh"))
-//	{
-	/*restore original sys_exit*/
-	sys_call_table[__NR_exit] = (unsigned long* ) original_sys_exit;
-//	}
-}
+	void cleanup_module(void)
+	{
+		/*restore original sys_exit*/
+		sys_call_table[__NR_exit] = (unsigned long* ) original_sys_exit;
+	}
