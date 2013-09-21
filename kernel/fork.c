db96ef5447d8ff3968e3e91da7828929557305e2
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-21 00:59:56 -0400

    Resurrect the tasklist_lock

--- kernel/fork.c
+++ kernel/fork.c
@@ -88,6 +88,9 @@ DEFINE_PER_CPU(unsigned long, process_counts) = 0;
 
 __cacheline_aligned DEFINE_RWLOCK(tasklist_lock);  /* outer */
 
+/*Export the tasklist_lock to any module with a GPL license*/
+EXPORT_SYMBOL_GPL(tasklist_lock);
+
 #ifdef CONFIG_PROVE_RCU
 int lockdep_tasklist_lock_is_held(void)
 {
