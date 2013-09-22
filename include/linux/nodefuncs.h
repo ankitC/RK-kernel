accd53feb545b4be7f1907391d314f7955f597ab
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-21 20:21:14 -0400

    Added the header for ps.c

---
+++ include/linux/nodefuncs.h
@@ -0,0 +1,10 @@
+typedef struct task_node
+{
+	int pid;
+	int prio;
+	char name[32];
+	struct task_node* next;
+
+}TASK_NODE;
+void add_node(TASK_NODE**, TASK_NODE*);
+void delete_linked_list (TASK_NODE**);
