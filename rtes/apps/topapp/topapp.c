9023c32a6801159e0f54e7fd46f9b8063a3454dc
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-22 17:35:51 -0400

    Adding the working app logic

--- rtes/apps/topapp/topapp.c
+++ rtes/apps/topapp/topapp.c
@@ -2,12 +2,52 @@
 #include <stdio.h>
 #include <unistd.h>                  /*  for sleep()  */
 #include <asm/unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <curses.h>
+#include <sys/syscall.h>
+
+#define LINELENGTH 43
+#define BUFF_SIZE(x) (x * LINELENGTH + 1)
+
+
+static sigset_t mask;
+static int exit_process = 0;
+
+int list_processes(char* buffer, int len)
+{
+	int retval = 0;
+	if ((retval = syscall(__NR_list_processes, buffer, len)) == 0)
+	{
+		printf("return %d\n", retval);
+		return 0;
+	}
+	else
+		return -1;
+}
+
+int count_processes()
+{
+	return syscall(__NR_count_processes);
+}
+
+void sig_int_handler()
+{
+//	sigprocmask(SIG_UNBLOCK, &mask, NULL);
+	exit_process = 1;
+	return;
+}
+
 
 int main(void) {
 
 	WINDOW * mainwin;
-
+	sigemptyset( &mask );
+	sigaddset(&mask, SIGTSTP);
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+	signal(SIGINT, sig_int_handler);
+	int i = (int) count_processes();
+	char* buffer = calloc( BUFF_SIZE(i), 1);
 
 	/*  Initialize ncurses  */
 
@@ -16,16 +56,28 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 
-
+	printf("Screen Initialized\n");
 	/*  Display "Hello, world!" in the centre of the
 	 *		screen, call refresh() to show our changes, and
 	 *			sleep() for a few seconds to get the full screen effect  */
-	while(true){
-	
+/*	while(1){
 		mvaddstr(13, 33, "Hello, world!");
 		refresh();
 		sleep(2);
+	}*/
+
+	while(!exit_process)
+	{
+		if (!list_processes(buffer, BUFF_SIZE(i)))
+		{
+			printw("%s",buffer);
+		}
+		refresh();
+		sleep(2);
+		clear();
 	}
+	free(buffer);
+
 
 	/*  Clean up after ourselves  */
 
