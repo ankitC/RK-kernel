28f390b1cf8a50695b174aa2cbad5c1285a5403a
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-23 19:02:27 -0400

    Finished the topapp code

--- rtes/apps/topapp/topapp.c
+++ rtes/apps/topapp/topapp.c
@@ -10,6 +10,8 @@
 #define LINELENGTH 43
 #define BUFF_SIZE(x) (x * LINELENGTH + 1)
 
+#define D(x)
+
 
 static sigset_t mask;
 static int exit_process = 0;
@@ -18,10 +20,7 @@ int list_processes(char* buffer, int len)
 {
 	int retval = 0;
 	if ((retval = syscall(__NR_list_processes, buffer, len)) == 0)
-	{
-		printf("return %d\n", retval);
 		return 0;
-	}
 	else
 		return -1;
 }
@@ -33,57 +32,45 @@ int count_processes()
 
 void sig_int_handler()
 {
-//	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 	exit_process = 1;
 	return;
 }
 
-
+/*Application for displaying current processes*/
 int main(void) {
 
 	WINDOW * mainwin;
+
+	/*Blocking signals to only service CLTR-C*/
 	sigemptyset( &mask );
 	sigaddset(&mask, SIGTSTP);
 	sigprocmask(SIG_BLOCK, &mask, NULL);
 	signal(SIGINT, sig_int_handler);
+	
 	int i = (int) count_processes();
 	char* buffer = calloc( BUFF_SIZE(i), 1);
 
 	/*  Initialize ncurses  */
-
 	if ( (mainwin = initscr()) == NULL ) {
 		fprintf(stderr, "Error initialising ncurses.\n");
 		exit(EXIT_FAILURE);
 	}
-
-	printf("Screen Initialized\n");
-	/*  Display "Hello, world!" in the centre of the
-	 *		screen, call refresh() to show our changes, and
-	 *			sleep() for a few seconds to get the full screen effect  */
-/*	while(1){
-		mvaddstr(13, 33, "Hello, world!");
-		refresh();
-		sleep(2);
-	}*/
-
+	
 	while(!exit_process)
 	{
 		if (!list_processes(buffer, BUFF_SIZE(i)))
 		{
 			printw("%s",buffer);
 		}
+
 		refresh();
-		sleep(2);
+		sleep(1);
 		clear();
 	}
+
+	/*Clean up after ourselves*/
 	free(buffer);
-
-
-	/*  Clean up after ourselves  */
-
 	delwin(mainwin);
 	endwin();
-	refresh();
-
 	return EXIT_SUCCESS;
 }
