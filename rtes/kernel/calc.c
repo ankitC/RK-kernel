e614b908118e49449100d2224fd7aa266be84040
Samyuktha Subramanian <samyukts@andrew.cmu.edu>
2013-09-24 18:56:35 -0400

    Final Calc

--- rtes/kernel/calc.c
+++ rtes/kernel/calc.c
@@ -1,33 +1,52 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
-#include <errno.h>
-
-/*asmlinkage int sys_calc(long first, long second, char operation){
-
-  printk(KERN_DEBUG "TestCalc\n");
-  }*/
-
+#define FIXED_SCALE 10000
+#define SATURATE 0x7FFFFFFF
 
 SYSCALL_DEFINE3(calc, long, first, long, second, char, operation)
 {
 	long result = 0;
 
+	printk(KERN_INFO "FIRST = %ld\n", first);
+	printk(KERN_INFO "SECOND = %ld\n", second);
+
 	switch(operation){
 
 		case '+':
+
+			if (first == SATURATE || second == SATURATE){
+				result = 0xDEADBEEF;
+				break;
+			}
 			result = first + second;
 			break;
 		case '-':
+
+			if (first == SATURATE || second == SATURATE){
+				result = 0xDEADBEEF;
+				break;
+			}
 			result = first - second;
 			break;
 		case '*':
+			if (first == SATURATE || second == SATURATE){
+				result = 0xDEADBEEF;
+				break;
+			}
 			result = first * second;
 			break;
 		case '/':
-			if (second == 0)
-				errno = EDOM;
+			first =  first * FIXED_SCALE;
+			printk(KERN_INFO "FIRST = %ld\n", first);
+			if (second == 0){
+				result = 0xDEADBEEF;
+				break;
+			}
+			printk(KERN_INFO "SECOND = %ld\n", second);
 			result = first / second;
+			printk(KERN_INFO "RESULT = %ld\n", result);
+			return result;
 			break;
 		default:
 			printk(KERN_WARNING "Operation not Supported.\n");
