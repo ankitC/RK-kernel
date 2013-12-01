57cd38b9af2bf2d1a0638224a3094629fb58e663
Ankit <ankit.ac.chheda@gmail.com>
2013-12-01 17:43:40 -0500

    Checking CPUs from 1 to N and shutting them down to save pow

--- rtes/kernel/energy_saving.c
+++ rtes/kernel/energy_saving.c
@@ -12,24 +12,28 @@
 #define TOTAL_CORES 4
 extern int guarantee;
 extern BIN_NODE* bin_head;
-
+extern spinlock_t bin_spinlock;
 /* Disabling unused CPUs for energy saving */
 
 void energy_savings(void)
 {
+
 	BIN_NODE* curr = bin_head;
 	int cpu_used[TOTAL_CORES] = {0, 0, 0, 0};
 	int i = 0;
+	unsigned long flags = 0;
 
 	if (guarantee)
 	{
+		spin_lock_irqsave(&bin_spinlock, flags);
 		while (curr)
 		{
 			cpu_used[curr->task->reserve_process->host_cpu]++;
 			curr = curr->next;
 		}
+		spin_unlock_irqrestore(&bin_spinlock, flags);
 
-		for (i = 0; i < TOTAL_CORES; i++)
+		for (i = 1; i < TOTAL_CORES; i++)
 		{
 			if (cpu_used[i] == 0)
 				cpu_down(i);
